e531f383865ab88f6b9d985ea364cb442f920ab7
btidor <btidor@mit.edu>
2021-09-04 07:33:45 +0000

    Testing Vercel ignored build step

--- fzf/heap.go
+++ fzf/heap.go
@@ -2,20 +2,7 @@ package fzf
 
 type Result struct {
 	Score int
-	Line  *string
-}
-
-type ResultHeap []Result
-
-func (r ResultHeap) Len() int           { return len(r) }
-func (r ResultHeap) Less(i, j int) bool { return r[i].Score < r[j].Score }
-func (r ResultHeap) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
-
-func (r *ResultHeap) Push(x interface{}) {
-	*r = append(*r, x.(Result))
-}
-
-func (r *ResultHeap) Pop() interface{} {
+	Line  *stri
 	old := *r
 	n := len(old)
 	x := old[n-1]
