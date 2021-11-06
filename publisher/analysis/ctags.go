6ec10a6c869d046f9884a3f2ef745b6335753da7
Benjamin Tidor <benjamin@tidor.net>
2021-11-06 15:15:36 -0700

    Publisher: fix ctags segfault

--- publisher/analysis/ctags.go
+++ publisher/analysis/ctags.go
@@ -5,7 +5,10 @@ import (
 )
 
 func ConstructCtagsIndex(a Archive) []byte {
-	cmd := exec.Command("ctags", "-f", "-", "--recurse", "--links=no", "--excmd=number")
+	cmd := exec.Command(
+		"ctags", "-f", "-", "--recurse", "--links=no", "--excmd=number",
+		"--exclude=*.json", // due to segfault on libcpanel-json-xs-perl test cases
+	)
 	cmd.Dir = a.Dir // paths are relative to this directory
 	out, err := cmd.Output()
 	if err != nil {
