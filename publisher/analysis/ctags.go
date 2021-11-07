e0b8a7e336361affb911c95363d1dff96201efa6
Benjamin Tidor <benjamin@tidor.net>
2021-11-06 19:16:52 -0700

    Ctags: exclude *.md files also

--- publisher/analysis/ctags.go
+++ publisher/analysis/ctags.go
@@ -8,7 +8,8 @@ func ConstructCtagsIndex(a Archive) []byte {
 	cmd := exec.Command(
 		"ctags", "-f", "-", "--recurse", "--links=no", "--excmd=number",
 		"--exclude=*.json",  // due to segfault on libcpanel-json-xs-perl test cases
-		"--exclude=*.patch", // tags are unnecessary + unmatchable
+		"--exclude=*.patch", // tags are unnecessary and garbled
+		"--exclude=*.md",    // verbose + not useful (?)
 	)
 	cmd.Dir = a.Dir // paths are relative to this directory
 	out, err := cmd.Output()
