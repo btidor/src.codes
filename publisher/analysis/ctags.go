1393552584e167a75d53110a9298bef14eaab02b
Benjamin Tidor <benjamin@tidor.net>
2021-11-06 17:45:34 -0700

    Ctags: exclude *.patch files

--- publisher/analysis/ctags.go
+++ publisher/analysis/ctags.go
@@ -7,7 +7,8 @@ import (
 func ConstructCtagsIndex(a Archive) []byte {
 	cmd := exec.Command(
 		"ctags", "-f", "-", "--recurse", "--links=no", "--excmd=number",
-		"--exclude=*.json", // due to segfault on libcpanel-json-xs-perl test cases
+		"--exclude=*.json",  // due to segfault on libcpanel-json-xs-perl test cases
+		"--exclude=*.patch", // tags are unnecessary + unmatchable
 	)
 	cmd.Dir = a.Dir // paths are relative to this directory
 	out, err := cmd.Output()
