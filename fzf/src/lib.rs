5acb5e2247b45ba26ff11061f8f50083c99250ed
Benjamin Tidor <benjamin@tidor.net>
2021-11-11 15:53:31 -0800

    Ctags: remove server

--- fzf/src/lib.rs
+++ fzf/src/lib.rs
@@ -3,11 +3,9 @@ mod directory;
 mod matcher;
 mod path_server;
 mod query;
-mod tag_server;
 
 pub use char_set::CharSet;
 pub use directory::{Directory, PChar, PathComponent};
 pub use matcher::Matcher;
 pub use path_server::PathServer;
 pub use query::{QChar, Query};
-pub use tag_server::TagServer;
