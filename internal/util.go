dee1fede5304a8cae15d3dd529d50210b41d1e43
Benjamin Tidor <benjamin@tidor.net>
2021-12-21 19:41:48 -0800

    Publisher: new download helper

--- internal/util.go
+++ internal/util.go
@@ -4,9 +4,13 @@ package internal
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
+	"path/filepath"
 	"strings"
 )
 
@@ -53,18 +57,59 @@ func HTTPError(w http.ResponseWriter, r *http.Request, code int) {
 // either a complete URL or a base URL followed by a sequence of path segments
 // as in URLWithPath.
 func DownloadFile(u *url.URL, s ...string) *bytes.Buffer {
-	r, err := http.Get(URLWithPath(u, s...).String())
+	us := URLWithPath(u, s...).String()
+	r, err := http.Get(us)
 	if err != nil {
 		panic(err)
 	}
 	defer r.Body.Close()
 
 	if r.StatusCode != 200 {
-		err = fmt.Errorf("could not download %s: HTTP %s", u.String(), r.Status)
+		err = fmt.Errorf("could not download %s: HTTP %s", us, r.Status)
 		panic(err)
 	}
 
 	var b bytes.Buffer
 	b.ReadFrom(r.Body)
 	return &b
+}
+
+// SaveFile downloads a URL to disk and panics if an error occurs or if the HTTP
+// request returns a non-200 status code. The operation is atomic: the file is
+// streamed to a temporary file which is then renamed into place. For
+// convenience, callers may pass either a complete URL or a base URL followed by
+// a sequence of path segments as in URLWithPath.
+func SaveFile(dest string, u *url.URL, s ...string) {
+	tmp, err := ioutil.TempFile(filepath.Dir(dest), "tmp-")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(tmp.Name())
+	defer tmp.Close()
+
+	us := URLWithPath(u, s...).String()
+	r, err := http.Get(us)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != 200 {
+		err = fmt.Errorf("could not download %s: HTTP %s", us, r.Status)
+		panic(err)
+	}
+
+	_, err = io.Copy(tmp, r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmp.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = os.Rename(tmp.Name(), dest)
+	if err != nil {
+		panic(err)
+	}
 }
