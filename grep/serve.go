1bea9dc6c08082fca6b4873d10c8e16018b22d31
Benjamin Tidor <benjamin@tidor.net>
2021-12-22 11:32:29 -0800

    Grep: add request timeout

--- grep/serve.go
+++ grep/serve.go
@@ -5,15 +5,15 @@ import (
 	"net/http"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/btidor/src.codes/internal"
 	"github.com/google/codesearch/index"
 	"github.com/google/codesearch/regexp"
 )
 
-var indexes = make(map[string][]string)
-
 func serve() {
+	var indexes = make(map[string][]string)
 	for distro := range distros {
 		matches, err := filepath.Glob(
 			filepath.Join(dataDir, "grep", distro, "*", "*.csi"),
@@ -22,59 +22,69 @@ func serve() {
 			panic(err)
 		}
 		if len(matches) < 1 {
-			panic("no indexes found for distro "+distro)
+			panic("no indexes found for distro " + distro)
 		}
 		indexes[distro] = matches
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var parts = strings.Split(r.URL.Path, "/")
-		if len(parts) != 2 {
-			// Invalid path
-			internal.HTTPError(w, r, 404)
-			return
-		}
-		distro := parts[1]
-		if distro == "" {
-			// Requests to `/` show a welcome message
-			fmt.Fprintf(w, "Hello from grep@dev!\n")
-			return
-		}
-		if _, ok := distros[distro]; !ok {
-			// Unknown distro
-			internal.HTTPError(w, r, 404)
-			return
-		}
 
-		var query = r.URL.Query().Get("q")
-		if query == "" {
-			// Missing query
-			internal.HTTPError(w, r, 400)
-			return
-		}
-
-		var grep = regexp.Grep{Stdout: w, Stderr: w}
-		re, err := regexp.Compile("(?m)" + query)
-		if err != nil {
-			// TODO: handle gracefully
-			panic(err)
-		}
-		grep.Regexp = re
-		var iquery = index.RegexpQuery(re.Syntax)
-		search(distro, grep, iquery)
-	})
+	var server = &http.Server{
+		Handler:      grepHandler{indexes},
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 60 * time.Second,
+	}
 	var err error
 	if certPath != "" || keyPath != "" {
 		fmt.Println("Listening on :443")
-		err = http.ListenAndServeTLS(":443", certPath, keyPath, nil)
+		server.Addr = ":443"
+		err = server.ListenAndServeTLS(certPath, keyPath)
 	} else {
 		fmt.Println("Listening on :5050")
-		err = http.ListenAndServe(":5050", nil)
+		server.Addr = ":5050"
+		err = server.ListenAndServe()
 	}
 	panic(err)
 }
 
-func search(distro string, grep regexp.Grep, iquery *index.Query) {
-	for _, name := range indexes[distro] {
+type grepHandler struct {
+	indexes map[string][]string
+}
+
+func (g grepHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var parts = strings.Split(r.URL.Path, "/")
+	if len(parts) != 2 {
+		// Invalid path
+		internal.HTTPError(w, r, 404)
+		return
+	}
+	distro := parts[1]
+	if distro == "" {
+		// Requests to `/` show a welcome message
+		fmt.Fprintf(w, "Hello from grep@dev!\n")
+		return
+	}
+	idxlist, ok := g.indexes[distro]
+	if !ok {
+		// Unknown distro
+		internal.HTTPError(w, r, 404)
+		return
+	}
+
+	var query = r.URL.Query().Get("q")
+	if query == "" {
+		// Missing query
+		internal.HTTPError(w, r, 400)
+		return
+	}
+
+	var grep = regexp.Grep{Stdout: w, Stderr: w}
+	re, err := regexp.Compile("(?m)" + query)
+	if err != nil {
+		// TODO: handle gracefully
+		panic(err)
+	}
+	grep.Regexp = re
+	var iquery = index.RegexpQuery(re.Syntax)
+	for _, name := range idxlist {
 		ix := index.Open(name)
 		for _, fileid := range ix.PostingQuery(iquery) {
 			path := filepath.Join(dataDir, "packages", distro, ix.Name(fileid))
